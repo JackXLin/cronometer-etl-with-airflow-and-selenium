223cab4792a5967dda1c9249ef3779752166d139
King-Derp <jack.llll@yahoo.co.uk>
2025-08-27 18:22:24 +0100

    fixed the issue there the processed_data.csv is not remove for refresh dag run

--- dags/remove_old_file.py
+++ dags/remove_old_file.py
@@ -4,9 +4,13 @@ def remove_old_files():
     download_dir = "/opt/airflow/csvs"
     file_path_biometrics = os.path.join(download_dir, 'biometrics.csv')
     file_path_dailysummary = os.path.join(download_dir, 'dailysummary.csv')
+    file_path_processed = os.path.join(download_dir, 'processed_data.csv')
     
     if os.path.exists(file_path_biometrics):
         os.remove(file_path_biometrics)
     
     if os.path.exists(file_path_dailysummary):
         os.remove(file_path_dailysummary)
+    
+    if os.path.exists(file_path_processed):
+        os.remove(file_path_processed)
